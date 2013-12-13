36f9164cca9147719fe26f6739c692a9f622518c
wragen22 <bradwrage@gmail.com>
2013-12-13 09:33:36 -0800

    First page scaffod

---
+++ index.php
@@ -0,0 +1,179 @@
+
+
+<?php include("includes/_header.php"); ?>
+
+<body>
+<div class="top-nav">
+  <div class="row">
+    <div class="large-12 columns">
+      <img class="logo" src="/assets/images/circuit-logo.svg">
+      <ul class="nav">
+        <li><a href="">Home</a></li>
+        <li><a href="">Home</a></li>
+        <li><a href="">Home</a></li>
+        <li><a href="">Home</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+<section class="wrapper-container">
+  <div class="row">
+    <div class="large-12 columns">
+      <h1 class="hero-title">Circuit Therapeutics</h1>
+      <h2 class="header-subtitle">The industry leader in optogenetics developing breakthrough therapeutic interventions transforming the field of drug discovery</h2>
+    </div>
+  <div class="hero-cta">
+    <a href="#" class="button [radius round] hero-cta">Learn More</a>
+  </div>
+  </div>
+</section>
+
+<!-- About Circuit TH -->
+
+<section class="about-circuit">
+  <div class="row">
+    <div class="large-12 columns">
+      <h2 class="section-header">Leader in Optogenics</h2>
+      <h3 class="section-subheader">We are incredibly exciting to see what technology has allowed us to do. With optogenics we have explored and found amazing possibilities.</h3>
+    </div>
+  </div>
+  <div class="row section-top">
+    <div class="large-4 columns">
+      <div class="about-item-image"></div>
+      <h3 class="about-item-title">Years of Research</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, ducimus ipsa placeat autem aut deleniti quos sunt quae veniam. Asperiores, repellendus, quia autem ex nihil ad consequuntur ab numquam dolore.</p>
+    </div>
+     <div class="large-4 columns">
+      <div class="about-item-image"></div>
+      <h3 class="about-item-title">Key Partners</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, ducimus ipsa placeat autem aut deleniti quos sunt quae veniam. Asperiores, repellendus, quia autem ex nihil ad consequuntur ab numquam dolore.</p>
+    </div>
+     <div class="large-4 columns">
+      <div class="about-item-image"></div>
+      <h3 class="about-item-title">Big Dreams</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, ducimus ipsa placeat autem aut deleniti quos sunt quae veniam. Asperiores, repellendus, quia autem ex nihil ad consequuntur ab numquam dolore.</p>
+    </div>
+  </div>
+  <hr />
+</section>
+
+<!-- How it works -->
+<section class="how-it-works">
+  <div class="row">
+    <div class="large-12 columns">
+      <h2 class="section-header">How it Works</h2>
+      <h3 class="section-subheader">Incredible advancements in neuroscience that has allowed for ways to control the human nervous system to beat genetic disorders and deficiencies.</h3>
+    </div>
+  </div>
+
+  <div class="row content-row">
+    <div class="large-6 columns">
+      <div class="half-image"></div>
+    </div>
+    <div class="large-6 columns research">
+      <h3>Years of Research</h3>
+      <p class="main-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, illo error natus quibusdam incidunt labore officia iusto facere ducimus repellendus ex fugit aperiam nulla deleniti quod quae doloremque numquam quo.</p>
+    </div>
+  </div>
+  <div class="row content-row">
+    <div class="large-6 columns">
+      <h3>Incredible Findings</h3>
+      <p class="main-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, illo error natus quibusdam incidunt labore officia iusto facere ducimus repellendus ex fugit aperiam nulla deleniti quod quae doloremque numquam quo.</p>
+
+    </div>
+    <div class="large-6 columns research">
+        <div class="half-image"></div>
+    </div>
+  </div>
+
+<hr />
+</section>
+
+<section class="the-team">
+  <div class="row">
+    <div class="large-12 columns">
+      <h2 class="section-header">The Team</h2>
+      <h3 class="section-subheader">Incredible advancements in neuroscience that has allowed for ways to control the human nervous system to beat genetic disorders and deficiencies.</h3>
+    </div>
+  </div>
+  <div class="row">
+    <div class="large-11 large-offset-1 columns">
+      <ul class="small-block-grid-4">
+      <li>
+        <div class="team-pic"></div>
+        <h4>Tom Sudoff</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+      </li>
+      <li><div class="team-pic"></div>
+        <h4>Tom Sudoff</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></li>
+      <li><div class="team-pic"></div>
+        <h4>Tom Sudoff</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></li>
+      <li><div class="team-pic"></div>
+        <h4>Tom Sudoff</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></li>
+      <li><div class="team-pic"></div>
+        <h4>Tom Sudoff</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></li>
+      <li><div class="team-pic"></div>
+        <h4>Tom Sudoff</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></li>
+      <li><div class="team-pic"></div>
+        <h4>Tom Sudoff</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></li>
+      <li><div class="team-pic"></div>
+        <h4>Tom Sudoff</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p></li>
+      </ul>
+    </div>
+  </div>
+  <hr />
+</section>
+
+<section class="in-the-press">
+  <div class="row">
+    <div class="large-12 columns">
+      <h2 class="section-header">The Press</h2>
+      <h3 class="section-subheader">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac elit id quam viverra faucibus non in mi. Fusce eu pellentesque mauris. Pellentesque quis accumsan nibh.</h3>
+    </div>
+  </div>
+
+  <div class="row row-margin">
+    <div class="large-4 columns large-offset-1">
+      <div class="image"></div>
+    </div>
+    <div class="large-6 columns">
+      <h5 class="quoted-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac elit id quam viverra faucibus non in mi. </h5>
+      <p class="source">John Smith, Merc Industries</p>
+    </div>
+  </div>
+
+
+  <div class="row row-margin">
+    <div class="large-4 columns large-offset-1">
+      <div class="image"></div>
+    </div>
+    <div class="large-6 columns">
+      <h5 class="quoted-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac elit id quam viverra faucibus non in mi. </h5>
+      <p class="source">John Smith, Merc Industries</p>
+    </div>
+  </div>
+
+
+  <div class="row row-margin">
+    <div class="large-4 columns large-offset-1">
+      <div class="image"></div>
+    </div>
+    <div class="large-6 columns">
+      <h5 class="quoted-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac elit id quam viverra faucibus non in mi. </h5>
+      <p class="source">John Smith, Merc Industries</p>
+    </div>
+  </div>
+
+</section>
+
+
+<?php include("includes/_footer.php"); ?>
+</body>
+</html>
