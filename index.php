bd55bdcb5264b3f62aedd083715f5eacf412a69a
brad <bradwrage@gmail.com>
2014-03-15 16:28:27 -0700

    Update

---
+++ index.php
@@ -0,0 +1,188 @@
+
+
+<?php include("includes/_header.php"); ?>
+
+
+<section class="wrapper-container">
+  <div class="row">
+    <div class="large-12 columns">
+      <h1 class="hero-title">Circuit Therapeutics</h1>
+      <h2 class="header-subtitle">The industry leader in optogenetics <br />developing breakthrough therapeutic interventions <br />transforming the field of drug discovery</h2>
+    </div>
+  <div class="centered-btn">
+    <a href="technology.php" class="secondary button radius alternate">Learn More</a>
+  </div>
+  </div>
+</section>
+
+<!-- About Circuit TH -->
+
+<section class="about-circuit">
+  <div class="row">
+    <div class="large-10 large-centered columns home-hero-spacing">
+      <h2 class="section-header">Leader in Optogenetics</h2>
+      <h5 class="section-subheader">Driving technological advances to gain new perspectives and develop transformational therapies.</h3>
+    </div>
+  </div>
+  <div class="row section-top">
+    <div class="large-4 columns about-category">
+      <img class="light-bulb" src="assets/images/light-bulb.png">
+      <h3 class="about-item-title">Groundbreaking Technology</h3>
+      <p>Innovative research to provide dramatic advances</p>
+    </div>
+     <div class="large-4 columns about-category">
+       <img class="partners" src="assets/images/partners.png">
+      <h3 class="about-item-title">Key Partners</h3>
+      <p>Much of our success is directly a result of our team and key partners that have poured their time and energy into our dream.</p>
+    </div>
+     <div class="large-4 columns about-category">
+       <img class="heart" src="assets/images/bubble.png">
+      <h3 class="about-item-title">Big Dreams</h3>
+      <p>Our goal is much greater than becoming a succesful company, but to see lives touched and changed forever.</p>
+    </div>
+  </div>
+</section>
+
+<!-- How it works -->
+<section class="how-it-works">
+  <div class="row">
+    <div class="large-10 large-centered columns">
+      <h2 class="section-header blue">Revolutionary Technology in Neuroscience</h2>
+      <h5 class="section-subheader">Bringing breakthroughs in controlling the human nervous system to beat genetic disorders and deficiencies.</h3>
+    </div>
+  </div>
+
+  <div class="row content-row">
+    <div class="large-6 columns">
+    <img class="" src="assets/images/scientist.jpg">
+    </div>
+    <div class="large-6 columns research">
+      <h3>Optogenetics</h3>
+      <p class="main-content">Circuit Therapeutics is the industry leader in optogenetics and is pioneering new treatments through the use of this groundbreaking technology.  Optogenetics has gained rapid acceptance globally and is being used by hundreds of laboratories worldwide</p>
+    </div>
+  </div>
+  <div class="row content-row hide-for-small">
+    <div class="large-6 columns">
+      <h3>Incredible Findings</h3>
+      <p class="main-content">Through region-specific injection of viral vectors containing cell-type specific promoters, opsins can be targeted with a high degree of specificity to particular neurons of interest. The opsin-activating light can be directed to specific locations in the brain, providing yet another region-specific targeting capacity of optogenetics.
+      </p></p>
+
+    </div>
+    <div class="large-6 columns research">
+        <img class="" src="assets/images/research.jpg">
+    </div>
+  </div>
+
+   <div class="row content-row show-for-small">
+    <div class="large-6 columns research">
+        <img class="research-pic" src="assets/images/research.jpg">
+    </div>
+    <div class="large-6 columns research">
+      <h3>Incredible Findings</h3>
+      <p class="main-content">Through region-specific injection of viral vectors containing cell-type specific promoters, opsins can be targeted with a high degree of specificity to particular neurons of interest. The opsin-activating light can be directed to specific locations in the brain, providing yet another region-specific targeting capacity of optogenetics.
+      </p></p>
+    </div>
+  </div>
+</section>
+
+<!-- <section class="the-team team">
+  <div class="row">
+    <div class="large-12 columns">
+      <h2 class="section-header">The Management Team</h2>
+      <h5 class="section-subheader">Beyond their expertise in optogenetics, each and every member of our team possesses a fierce desire to make a previously unimaginable difference to the lives of those suffering from disorders of the nervous system. This ambition is what drives the team and creates the incredibly exciting environment at Circuit Therapeutics. </h3>
+    </div>
+  </div>
+  <div class="row">
+    <div class="large-12 columns">
+      <ul class="large-block-grid-3 alternate-grid">
+      <li>
+        <div class="person-container">
+          <img class="advisor-pic" src="assets/images/team/fred-moll-small.png">
+            <div class="right-about">
+             <h5 class="name">Fred Moll, <span>MD, PhD</span></h5>
+            <p>CEO and Chairman of the Board</p>
+          </div>
+        </div>
+      </li>
+      <li> 
+        <div class="person-container">
+         <img class="advisor-pic" src="assets/images/team/karoly-small.png">
+            <div class="right-about">
+            <h5 class="name">Karoly Nikolich, <span>PhD</span></h5>
+            <p>Founder of Circuit Therapeutics </p>
+
+          </div>
+        </div>
+      </li>
+      <li>
+         <div class="person-container">
+        <img class="advisor-pic" src="assets/images/team/mike-small.png">
+            <div class="right-about">
+            <h5 class="name">Michael Kaplitt, <span>MD,PhD</span></h5>
+            <p>Chief Scientific Officer and Sr. Vice President</p>
+          </div>
+        </div>
+      </li>
+
+           <li>
+        <div class="person-container">
+          <img class="advisor-pic" src="assets/images/team/steven-small.png">
+            <div class="right-about">
+            <h5 class="name">Stephen Braithwaite, <span>PhD</span></h5>
+            <p>Sr. Vice President Research</p>
+          </div>
+        </div>
+      </li>
+        <li>
+        <div class="person-container">
+           <img class="advisor-pic" src="assets/images/team/new-member.png">
+            <div class="right-about">
+            <h5 class="name">Dan Andersen <span></span></h5>
+            <p>Vice President Engineering</p>
+          </div>
+        </div>
+      </li>
+          <li>
+        <a class="new-job"href="jobs.php"><div class="person-container">
+           <img class="advisor-pic" src="assets/images/team/new-member.png">
+            <div class="right-about">
+            <h5 class="name">You <span></span></h5>
+            <p>Join us to become the next member of the team.</p>
+          </div>
+        </div>
+        </a>
+      </li>
+      </ul>
+      <div class="centered-btn see-the-team">
+    <a href="about.php" class="button centered-btn radius">Learn More</a>
+  </div>
+    </div>
+  </div>
+</section> -->
+
+<section class="in-the-press">
+  <div class="row">
+    <div class="large-10 large-centered columns">
+      <h2 class="section-header blue">The Press</h2>
+      <h5 class="section-subheader ">Circuit Therapeutics and Boehringer Ingelheim announce signature of a research partnership agreement to discover new approaches to treating psychiatric disorders.</h3>
+    </div>
+  </div>
+  <div class="row row-margin mobile-left">
+    <div class="large-4 columns large-offset-1 news-snippet">
+      <img class="" src="assets/images/logos/collaboration.png">
+    </div>
+    <div class="large-6 columns">
+      <h5 >Boehringer Ingelheim and Circuit Therapeutics have entered into a research collaboration agreement for the discovery of new ways of treating psychiatric disorders. </h5>
+      <p class="source">MENLO PARK, California and INGELHEIM, Germany, December 18th, 2013</p>
+    </div>
+  </div>
+  <div class="centered-btn">
+    <a href="news.php" class="button centered-btn radius">Learn More</a>
+  </div>
+</section>
+</section>
+
+
+<?php include("includes/_footer.php"); ?>
+</body>
+</html>
